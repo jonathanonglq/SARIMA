afdd78174230fdb873af8907ef03fb0f1cea02dd
Jonathan Ong <42633702+jonathanonglq@users.noreply.github.com>
2025-03-17 01:11:34 +0800

    streamlit test

--- SARIMA.py
+++ SARIMA.py
@@ -156,9 +156,9 @@ if isinstance(df, pd.DataFrame) and steps and seasonal_period and ind:
 
             # Define the range of values for p, d, q, P, D, Q, and m
             p_values = range(0, 3)          # Autoregressive order
-            d_values = range(0, 3)          # Differencing order
+            d_values = range(0, 2)          # Differencing order
             q_values = range(0, 3)          # Moving average order
-            P_values = range(0, 1)          # Seasonal autoregressive order
+            P_values = range(0, 2)          # Seasonal autoregressive order
             D_values = range(0, 1)          # Seasonal differencing order
             Q_values = range(0, 1)          # Seasonal moving average order
             m_values = [seasonal_period]    # Seasonal period
